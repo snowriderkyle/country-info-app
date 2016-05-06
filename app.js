a52b2d38b35e5713964c7ac76c5841458bb3ac3e
Kyle van Til <snowrider_kyle@hotmail.com>
2016-05-06 11:26:48 +0200

    Added reading module

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-var countryName = process.argv
+var countryName = process.argv[2]
+var fs = require ('fs');
+
+fs.readFile( './countries.json', function( error, data ) {
+	if ( error ){
+		console.log("Something is up! Details: " + error)
+	}
+
+	var jasondata = JSON.parse (filedata) 
+
+})
