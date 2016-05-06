85982b56e587aed4c3af23b40a7cc23910aa27c0
Kyle van Til <snowrider_kyle@hotmail.com>
2016-05-06 11:30:54 +0200

    Test run

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var countryName = process.argv[2]
 var fs = require ('fs');
 
 fs.readFile( './countries.json', function( error, data ) {
+	//Make sure errors are visible in console
 	if ( error ){
 		console.log("Something is up! Details: " + error)
 	}
-
+	// Store our filedata in var as json
 	var jasondata = JSON.parse (filedata) 
-
+console.log (jsondata)
 })
